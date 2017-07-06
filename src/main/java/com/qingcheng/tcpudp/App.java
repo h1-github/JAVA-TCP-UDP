af1d50eed579850b34fc92c805bf725d62d2125a
wanghuan <hunter.v.wang@gmail.com>
2017-07-06 16:27:44 +0800

    init

---
+++ src/main/java/com/qingcheng/tcpudp/App.java
@@ -0,0 +1,15 @@
+package com.qingcheng.tcpudp;
+
+import utils.L;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        L.d("Application Starting");
+    }
+}
---
+++ src/main/java/utils/L.java
@@ -0,0 +1,37 @@
+package utils;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Created by wanghuan on 2017/7/6.
+ */
+public class L {
+
+    private static Logger logger = LogManager.getLogger();
+
+    public static void f(String message){
+        logger.fatal(message);
+    }
+
+    public static void e(String message){
+        logger.error(message);
+    }
+
+    public static void w(String message){
+        logger.warn(message);
+    }
+
+    public static void i(String message){
+        logger.info(message);
+    }
+
+    public static void d(String message){
+        logger.debug(message);
+    }
+
+    public static void t(String message){
+        logger.trace(message);
+    }
+
+}
